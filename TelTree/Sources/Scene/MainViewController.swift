b100c692f24a74a7be75f4357c342295de5b6037
circle08 <hawon9781@naver.com>
2024-07-16 13:37:22 +0900

    프로젝트 세팅

--- TelTree/Sources/Scene/MainViewController.swift
+++ TelTree/Sources/Scene/MainViewController.swift
@@ -1,3 +1,23 @@
 import UIKit
+import SnapKit
+import Then
 
-class MainViewController: BaseViewController {}
+class MainViewController: BaseViewController {
+    
+    let label = UILabel().then {
+        $0.text = "하원"
+        $0.textColor = TelTreeAsset.main.color
+    }
+    override func addView() {
+        self.view.addSubview(label)
+    }
+    override func setLayout() {
+        label.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+
+    override func configureViewController() {
+        self.view.backgroundColor = .red
+    }
+}
